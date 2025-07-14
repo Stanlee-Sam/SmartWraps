c6e745063602a08578de9f05002752b091f26fef
Stanley Amunze <stanleyamunze03@gmail.com>
2025-07-14 07:46:20 +0300

    Header Complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="bg-[#FF66C4] flex flex-row justify-between items-center p-2">
+      <div>
+        <img
+          className="w-20 h-20"
+          src="../../src/assets/Images/heroLogo.png"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-row gap-3">
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black ">
+          Home
+        </button>
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+          About Us
+        </button>
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+          Products
+        </button>
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+          Contact Us
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
