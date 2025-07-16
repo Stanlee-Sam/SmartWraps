bddc078c98fdeadf0a203f859226857fa28f7bf2
Stanley Amunze <stanleyamunze03@gmail.com>
2025-07-16 09:03:03 +0300

    Hero Section Complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-[#FF66C4] flex flex-row justify-between items-center p-2">
+    <div className="bg-[#FF66C4] flex flex-row justify-between items-center p-2 z-50 sticky top-0 shadow-md">
       <div>
         <img
-          className="w-18 h-18"
+          className="w-18 h-18 md:w-20 md:h-20"
           src="../../src/assets/Images/heroLogo.png"
           alt=""
         />
       </div>
-      <div className="flex flex-row gap-3">
-        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black ">
+      <div className="flex flex-row gap-3 sm:gap-3 md:gap-4">
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] sm:text-base md:text-lg font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black  transition-colors duration-300 ease-in-out active:bg-[#59bdf1] active:text-black">
           Home
         </button>
-        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] sm:text-base md:text-lg font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black transition-colors duration-300 ease-in-out active:bg-[#59bdf1] active:text-black">
           About Us
         </button>
-        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] sm:text-base md:text-lg font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black transition-colors duration-300 ease-in-out active:bg-[#59bdf1] active:text-black">
           Products
         </button>
-        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black">
+        <button className="border-2 border-black p-2 rounded-md bg-black text-white text-[15px] sm:text-base md:text-lg font-bold cursor-pointer hover:bg-[#59BDF1] hover:border-black hover:text-black transition-colors duration-300 ease-in-out active:bg-[#59bdf1] active:text-black">
           Contact Us
         </button>
       </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-lvh relative">
-      <div className="bg-cover bg-center md:bg-fill">
+    <div className="relative">
+      <div className="h-[calc(100svh-4rem) md:h-[calc(100vh-80px)] w-full overflow-hidden
+      ">
         <img
-          className="h-svh w-full"
+          className="h-full w-full object-cover object-center brightness-35 "
           src="../../src/assets/Images/hero.jpg"
           alt=""
         />
       </div>
-      <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-2">
-        <h1>SmartWraps</h1>
-        <p>Pre-Covered School Books And Stationery Delivered to You</p>
-        <button>Get Started</button>
+      <div className=" flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-2">
+        <h1 className="text-4xl md:text-[96px] font-bold text-white drop-shadow-lg  text-center " style={{fontFamily:"'Kaushan Script', cursive"}} >SmartWraps</h1>
+        <p className="text-center text-[15px] md:text-[20px] font-bold text-white drop-shadow-lg " style={{fontFamily : "'Bree Serif', serif"}}>Pre-Covered School Books And Stationery Delivered to You</p>
+        <button className="mt-4 px-6 py-2 md:px-8 md:py-3 bg-[#FF66C4] text-black font-bold rounded-lg hover:bg-[#59BDF1] hover:text-white transition-colors duration-300 border-2 border-black shadow-lg active:scale-95">Get Started</button>
       </div>
     </div>
   );
