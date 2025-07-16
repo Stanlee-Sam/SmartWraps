04dbb74648903d537e2602e986ce20bb38390195
Stanley Amunze <stanleyamunze03@gmail.com>
2025-07-16 09:03:30 +0300

    Hero Section complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className=" flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-2">
         <h1 className="text-4xl md:text-[96px] font-bold text-white drop-shadow-lg  text-center " style={{fontFamily:"'Kaushan Script', cursive"}} >SmartWraps</h1>
         <p className="text-center text-[15px] md:text-[20px] font-bold text-white drop-shadow-lg " style={{fontFamily : "'Bree Serif', serif"}}>Pre-Covered School Books And Stationery Delivered to You</p>
-        <button className="mt-4 px-6 py-2 md:px-8 md:py-3 bg-[#FF66C4] text-black font-bold rounded-lg hover:bg-[#59BDF1] hover:text-white transition-colors duration-300 border-2 border-black shadow-lg active:scale-95">Get Started</button>
+        <button className="cursor-pointer mt-4 px-6 py-2 md:px-8 md:py-3 bg-[#FF66C4] text-black font-bold rounded-lg hover:bg-[#59BDF1] hover:text-white transition-colors duration-300 border-2 border-black shadow-lg active:scale-95">Get Started</button>
       </div>
     </div>
   );
